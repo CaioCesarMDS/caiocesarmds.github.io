ac8cb32b2c09385093913acb6ee8e1b2b55108d4
CaioCesarMDS <caiocesarsts@gmail.com>
2024-05-05 22:17:50 -0300

    feat: refactoring styles and improving the slider

--- src/js/slider-image.js
+++ src/js/slider-image.js
@@ -1,12 +1,42 @@
-const list = document.querySelector(".slider");
-const items = list.querySelectorAll(".item");
+const categories = document.querySelectorAll(".categories button");
+const slider = document.querySelector(".slider");
+const items = slider.querySelectorAll(".item");
+const stadardCategory = slider.querySelectorAll(".item.languages").length;
 
+let lengthItems = stadardCategory;
+let itemsSlider = [0];
 let active = 0;
-let lengthItems = items.length - 1;
+
+categories.forEach((category) => {
+    category.addEventListener("click", () => {
+        selectCategory(category);
+        filterByActiveCategory();
+    });
+});
+
+function selectCategory(category) {
+    categories.forEach((category) => {
+        category.classList.remove("active");
+    });
+    category.classList.add("active");
+}
+
+function filterByActiveCategory() {
+    const activeCategory = document.querySelector("button.active").value;
+    itemsSlider = [];
+    for (let i = 0; i <= items.length - 1; i++) {
+        const itemClass = items[i].classList.value.split(" ")[1];
+        if (activeCategory === itemClass) {
+            itemsSlider.push(i);
+        }
+    }
+    active = itemsSlider[0];
+    lengthItems = itemsSlider[itemsSlider.length - 1];
+}
 
 function nextSlider() {
     if (active + 1 > lengthItems) {
-        active = 0;
+        active = itemsSlider[0];
     } else {
         active++;
     }
@@ -15,45 +45,13 @@ function nextSlider() {
 
 function reloadSlider() {
     let checkLeft = items[active].offsetLeft;
-    list.style.left = -checkLeft + "px";
+    slider.style.left = -checkLeft + "px";
 }
 
-setInterval(() => {
-    nextSlider();
-}, 3000);
+function startSlider() {
+    setInterval(() => {
+        nextSlider();
+    }, 3000);
+}
 
-// Categories buttons
-const categories = document.querySelectorAll(".categories button");
-
-categories.forEach((category) => {
-    category.addEventListener("click", () => {
-        categories.forEach((category) => {
-            category.classList.remove("active");
-        });
-        category.classList.add("active");
-    });
-});
-
-// Filter by categories
-const languages = document.querySelectorAll(".languages");
-const frameworks = document.querySelectorAll(".frameworks");
-const tools = document.querySelectorAll(".tools");
-
-frameworks.forEach((framework) => (framework.style.display = "none"));
-tools.forEach((tool) => (tool.style.display = "none"));
-
-const itens = [languages, frameworks, tools];
-
-categories.forEach((category) => {
-    category.addEventListener("click", () => {
-        itens.forEach((item) => {
-            item.forEach((element) => {
-                const elementClass = element.className.split(" ")[1];
-                element.style.display = "none";
-                if (category.value === elementClass) {
-                    element.style.display = "block";
-                }
-            });
-        });
-    });
-});
+startSlider();
